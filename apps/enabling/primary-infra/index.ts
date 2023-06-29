0d2d669a74eda998d72c2bbcf3b42c1631ab3355
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-29 19:58:10 -0400

    add new desc

--- apps/enabling/primary-infra/index.ts
+++ apps/enabling/primary-infra/index.ts
@@ -1,9 +1,9 @@
-import * as pulumi from "@pulumi/pulumi"
-import * as aws from "@pulumi/aws"
-import * as awsx from "@pulumi/awsx"
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("hackathon-ephemeral-bucket")
+const bucket = new aws.s3.Bucket("ephemeral-env-bucket");
 
 // Export the name of the bucket
-export const bucketName = bucket.id
+export const bucketName = bucket.id;
