00198e1fab2a31a6af48e0047f696625b7fa89d0
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-30 07:25:59 -0400

    config

--- apps/enabling/primary-infra/index.ts
+++ apps/enabling/primary-infra/index.ts
@@ -5,20 +5,3 @@ const bucketName = "ephemeral-bucket";
 
 // Create an AWS resource (S3 Bucket)
 let bucket = new aws.s3.Bucket(bucketName, {});
-
-// Create an S3 bucket policy to allow public deletion
-let deletionPolicy = new aws.s3.BucketPolicy(`${bucketName}Policy`, {
-  bucket: bucket.id,
-  policy: {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Sid: "PublicDelete",
-        Effect: "Allow",
-        Principal: "*",
-        Action: "s3:DeleteObject",
-        Resource: pulumi.interpolate`${bucket.arn}/*`,
-      },
-    ],
-  },
-});
