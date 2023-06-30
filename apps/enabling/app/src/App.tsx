1e2f4b380c1788f6ffc761aca8e907e1e09b35cc
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-30 05:19:03 -0400

    Test Ephemeral Env (#1)
    * test pr
    
    * update secrets
    
    * test change
    
    * test change
    
    * test change
    
    * test change
    
    * test change

--- apps/enabling/app/src/App.tsx
+++ apps/enabling/app/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return <div className="App">Zen Temp App</div>
+  return <div className="App">Zen Temp Appp</div>;
 }
 
-export default App
+export default App;
