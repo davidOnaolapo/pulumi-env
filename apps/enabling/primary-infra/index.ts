4e38ce4e2b2766e81a880e588c1702845f3f90c4
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-29 23:05:09 -0400

    test change

--- apps/enabling/primary-infra/index.ts
+++ apps/enabling/primary-infra/index.ts
@@ -5,5 +5,5 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("ephemeral-env-bucket");
 
-// Export the name of the bucketx
+// Export the name of the bucket
 export const bucketName = bucket.id;
