fc61986829c338ba0254c4de4580bbddc50f4dce
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-30 08:29:19 -0400

    config

--- apps/enabling/primary-infra/index.ts
+++ apps/enabling/primary-infra/index.ts
@@ -1,7 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as command from "@pulumi/command";
 
 const bucketName = "ephemeral-buckett";
+
+// Build the application
+const buildApp = new command.local.Command("build-app", {
+  create: "npm run build", // replace with your build command
+  dir: `${__dirname}/app-directory`, // replace with your application directory
+});
 
 // Create an AWS resource (S3 Bucket)
 let bucket = new aws.s3.Bucket(bucketName, {});
