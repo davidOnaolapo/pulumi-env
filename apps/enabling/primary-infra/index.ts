035beb7b06c700f39b47f0ed5bec3b9ff6de93db
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2023-06-30 07:27:37 -0400

    test infra change

--- apps/enabling/primary-infra/index.ts
+++ apps/enabling/primary-infra/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const bucketName = "ephemeral-bucket";
+const bucketName = "ephemeral-buckett";
 
 // Create an AWS resource (S3 Bucket)
 let bucket = new aws.s3.Bucket(bucketName, {});
